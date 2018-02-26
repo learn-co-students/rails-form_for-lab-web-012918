d4a04dd0406c8d2c64d6f76f9ef783b71fba28c5
zitro <bryan@bryanortiz.com>
2018-02-26 15:15:37 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+resources :students, except: :destroy
+resources :school_classes, except: :destroy
 end
